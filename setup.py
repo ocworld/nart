4dfec4d415a4ab6c8fd1980fa8b88d59f2e481c9
Keunhyun Oh <ocworld@gmail.com>
2019-03-01 20:45:16 +0900

    1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nart',
-    version='1.0.dev2',
+    version='1.0',
     description='NART: Naver RealTime Keyword',
     author='Keunhyun Oh',
     author_email='ocworld@gmail.com',
